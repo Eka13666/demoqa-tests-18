08eb09a38d638581b3d388371bbf2fa593483fb6
eka <katrinmadness17@gmail.com>
2023-03-16 16:31:25 +0300

    added RegistrationWithFakerTests

---
+++ src/test/java/tests/RegistrationWithFakerTests.java
@@ -0,0 +1,37 @@
+package tests;
+import org.junit.jupiter.api.Test;
+import static tests.TestData.*;
+import static tests.TestData.userName;
+
+
+public class RegistrationWithFakerTests extends TestBase{
+    @Test
+    void fillFormTest() {
+      registrationPage.openPage()
+              .setFirstName(userName)
+              .setLastName(userLastName)
+              .setEmail(userEmail)
+              .setGender(userGender)
+              .setPhone(userPhone)
+              .setBirthDate(userBirth_day, userBirth_month, userBirth_year)
+              .setSubjects(userSubjects)
+              .setHobbies(userHobbies)
+              .setPicture(userPictureLocation)
+              .setAddress(userAddress)
+              .setState(userState)
+              .setCity(userCity)
+              .clickSubmit();
+
+      registrationPage.verifyResultsModalAppears()
+              .verifyResult("Student Name", userName + " " + userLastName)
+              .verifyResult("Student Email", userEmail)
+              .verifyResult("Gender", userGender)
+              .verifyResult("Mobile", userPhone)
+              .verifyResult("Date of Birth", userBirth_day + " " + userBirth_month + "," + userBirth_year)
+              .verifyResult("Subjects", userSubjects)
+              .verifyResult("Hobbies", userHobbies)
+              .verifyResult("Address", userAddress)
+              .verifyResult("State and City", userState + " " + userCity);
+
+    }
+}
--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -1,4 +1,38 @@
 package tests;
 
+import com.github.javafaker.Faker;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+
 public class TestData {
+
+  public static Faker faker = new Faker();
+
+  public static Date fakerDateOfBirthday = faker.date().birthday();
+  public static String userSubjects = faker.options().option("Accounting", "Maths", "Arts", "English", "Physics", "Chemistry",
+          "Computer Science", "Economics", "Social Studies", "History", "Civics", "Commerce", "Hindi", "Biology");
+
+  public static String userHobbies = faker.options().option("Reading", "Sports", "Music");
+
+  public static Map<String, String[]> mapStateWithCity = Map.of(
+          "NCR", new String[]{"Delhi", "Gurgaon", "Noida"},
+          "Uttar Pradesh", new String[]{"Agra", "Lucknow", "Merrut"},
+          "Haryana", new String[]{"Karnal", "Panipat"},
+          "Rajasthan", new String[]{"Jaipur", "Jaiselmer"});
+
+  static String userName = faker.name().firstName(),
+          userLastName = faker.name().lastName(),
+          userEmail = faker.internet().emailAddress(),
+          userGender = faker.demographic().sex(),
+          userPhone = 8 + faker.phoneNumber().subscriberNumber(9),
+          userBirth_day = (new SimpleDateFormat("d", Locale.ENGLISH)).format(fakerDateOfBirthday),
+          userBirth_month = (new SimpleDateFormat("MMMM", Locale.ENGLISH)).format(fakerDateOfBirthday),
+          userBirth_year = (new SimpleDateFormat("y", Locale.ENGLISH)).format(fakerDateOfBirthday),
+          userPictureLocation = "file.png",
+          userAddress = faker.address().streetAddress(),
+          userState = faker.options().option(mapStateWithCity.keySet().toArray()).toString(),
+          userCity = faker.options().option(mapStateWithCity.get(userState));
 }
